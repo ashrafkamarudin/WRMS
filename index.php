749a6503a746e459478e64fe1c57439353cc2477
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2020-01-27 17:15:14 +0800

    test

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 session_start();
 
 //User Autoloader
-require_once 'libs/router.php';
-require_once 'libs/Controller.php';
-require_once 'libs/View.php';
-require_once 'libs/Model.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/router.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/Controller.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/View.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/Model.php';
 
 //Library
-require_once 'libs/Database.php';
-require_once 'libs/auth/authregister.php';
-require_once 'libs/auth/authlogin.php';
-require_once 'libs/Flash.php';
-require_once 'libs/Test.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/Database.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/auth/authregister.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/auth/authlogin.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/Flash.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'libs/Test.php';
 
-require_once 'config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'config.php';
 
 //Testing
-require_once 'test/ConsoleWrite.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'test/ConsoleWrite.php';
 
 $app = new Route();
 
